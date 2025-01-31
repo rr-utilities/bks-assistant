3eabd769dd645265ff40fbe7ee7811bbb5355abc
rravinthrarasa <rosshan2007@icloud.com>
2025-02-01 00:43:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 var clickcounter1 = 0;
 var messages1 = [
-    '<a href="prufung-1.html">GF M (27.01)</a>',
+    '<a href="https://schulnetz.bks-campus.ch/loginto.php?mode=0&lang=" target="_blank">schulNetz</a>',
+    '<a href="https://cloud.bks-campus.ch/index.php" target="_blank">NextCloud</a>',
+    '<a href="http://webmail.bks-campus.ch" target="_blank">Outlook</a>',
+    '<a href="https://www.gr.ch/DE/institutionen/verwaltung/ekud/ahb/wvb/muenzmuehle/menueplan/Seiten/default.aspx" target="_blank">Menüplan</a>',
+    '<a href="https://portal.bks-campus.ch" target="_blank">Portal</a>',
     ];
   
-function weiterePrufung() {
-    document.getElementById("weiterePrufung").innerHTML = messages1[clickcounter1 % messages1.length];
+function BKSLinks() {
+    document.getElementById("BKSLinks").innerHTML = messages1[clickcounter1 % messages1.length];
     clickcounter1 += 1;
 }
 
 var clickcounter2 = 0;
 var messages2 = [
-    '<a href="https://quizlet.com/user/opus_magnum/folders/spanisch-prufungsvorbereitung-20012025?i=5wbo8d&x=1xqY">SF SPA (20.01)</a>',
-    '<a href="https://quizlet.com/user/opus_magnum/folders/franzosisch-prufungsvorbereitung-29012025?i=5wbo8d&x=1xqY">GF F (29.01)</a>',
+    '<a href="https://quizlet.com/user/bks_chur/folders/medias-in-res">Latein</a>',
+    '<a href="https://quizlet.com/user/bks_chur/folders/spanisch-puente-nuevo-1-2">Spanisch</a>',
+    '<a href="https://quizlet.com/user/bks_chur/folders/franzosisch-le-cours-intensif-1-2-3">Französisch</a>',
+    '<a href="https://quizlet.com/user/bks_chur/folders/italienisch-espresso-ragazzi-1-2-3">Italienisch</a>',
+    '<a href="https://quizlet.com/user/bks_chur/folders/romanisch-sursilvan-en-lingia-directa-1-2">Romanisch Surilvan</a>',
+    '<a href="https://quizlet.com/user/bks_chur/folders/englisch-headway-intermediate-ready-for-c1-advanced">Englisch</a>',
     ];
 
-function Sprachprufung() {
-    document.getElementById("Sprachprufung").innerHTML = messages2[clickcounter2 % messages2.length];
+function Sprachen() {
+    document.getElementById("Sprachen").innerHTML = messages2[clickcounter2 % messages2.length];
     clickcounter2 += 1;
-}
-
-var clickcounter3 = 0;
-var messages3 = [
-    '<a href="hausaufgaben">FF L</a>',
-    ];
-
-function Hausaufgaben() {
-    document.getElementById("Hausaufgaben").innerHTML = messages3[clickcounter3 % messages3.length];
-    clickcounter3 += 1;
 }
